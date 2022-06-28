b2bf960364bed3645b7bec3f9956a18b58a49d27
樊心吕 <9493342+fanxinlv@user.noreply.gitee.com>
2022-06-28 21:00:38 +0800

    完成用户列表功能的开发

---
+++ src/components/welcome.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>welcome2
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyWelcome'
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -16,6 +16,11 @@ Vue.use(ElementUI);
 Vue.config.productionTip =false
 
 axios.defaults.baseURL = "http://127.0.0.1:8888/api/private/v1/"
+axios.interceptors.request.use(config => {
+  config.headers.Authorization = window.localStorage.getItem('token')
+  console.log(config)
+  return config
+})
 Vue.prototype.$http = axios
 
 new Vue({
